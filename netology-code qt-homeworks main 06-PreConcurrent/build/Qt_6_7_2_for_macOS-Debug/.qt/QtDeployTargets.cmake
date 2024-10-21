149307a70686388908c80c210b6b8a50d4263fc1
Alisa <alisamun2006@gmail.com>
2024-10-21 20:54:00 +0300

    cc.01

---
+++ netology-code qt-homeworks main 06-PreConcurrent/build/Qt_6_7_2_for_macOS-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,2 @@
+set(__QT_DEPLOY_TARGET_Concurrent_FILE /Users/alisamun/Downloads/netology-code qt-homeworks main 06-PreConcurrent/build/Qt_6_7_2_for_macOS-Debug/Concurrent)
+set(__QT_DEPLOY_TARGET_Concurrent_TYPE EXECUTABLE)
